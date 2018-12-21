e23b62ad26c162cb0cd28ed2082b15527492f314
svkote <kotekofly@gmail.com>
2018-12-22 01:55:46 +0300

    Initial commit

---
+++ lesson1/script.js
@@ -0,0 +1,20 @@
+// var tempCel = prompt('Введите температуру по Цельсию');
+
+window.addEventListener('DOMContentLoaded', function () {
+        // #1
+    var tempCel = document.getElementById("tempCel"),
+        tempFar = document.getElementById("tempFar");
+
+
+        tempCel.addEventListener('change', () => {
+            tempFar.value = (9 / 5) * tempCel.value + 32;
+        });
+
+        // #3
+    var admin = document.getElementById('nameAdmin');
+    var name;
+    name = "Василий";
+    admin.value = name;
+
+
+});
