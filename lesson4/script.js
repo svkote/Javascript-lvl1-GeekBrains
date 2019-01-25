7f9384ac90d785af7fd18baede9e9590661c8ac8
svkote <kotekofly@gmail.com>
2019-01-25 13:58:43 +0300

    hw

--- lesson4/script.js
+++ lesson4/script.js
@@ -22,4 +22,24 @@ window.addEventListener('DOMContentLoaded', function () {
 
     console.log(primeArr);
 
+
+    var max = 999;
+        var digit = {
+          number: prompt('Введите число от 0 до 999'),
+          units: 0,
+          tens: 0,
+          hundreds: 0,
+        };
+        if (digit.number <= 9){
+          digit.units = digit.number;
+        } else if (digit.number <= 999){
+          digit.units = Math.floor(digit.number % 10);
+          digit.tens = Math.floor(digit.number / 10 % 10);
+          digit.hundreds = Math.floor(digit.number / 100 % 10);
+        }else {
+            digit.number = 0;
+            console.log('Вы ввели число за диапазоном 0 - 999');
+        }
+        console.log(digit);
+
 });
